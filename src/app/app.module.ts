aa8a4f575e236ef65fafccc006b1303b3f7ea4be
Carlos ACZ <lsca.cacz@gmail.com>
2024-04-15 10:35:54 -0600

    seccion 3 - diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRountingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotpageFoundComponent } from './page/notpage-found/notpage-found.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { Grafica1Component } from './page/grafica1/grafica1.component';
+import { ProgressComponent } from './page/progress/progress.component';
+import { PagesComponent } from './page/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotpageFoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRountingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page/notpage-found/notpage-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notpage-found',
+  templateUrl: './notpage-found.component.html',
+  styleUrls: ['./notpage-found.component.css']
+})
+export class NotpageFoundComponent {
+
+  year = new Date().getFullYear();
+  
+}
