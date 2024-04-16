0f272248bbdec0cca8c78641dfd335f78dc1c2af
Carlos ACZ <lsca.cacz@gmail.com>
2024-04-16 10:30:38 -0600

    Seccion 4 - Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modulos
 import { AppRountingModule } from './app-routing.module';
+import { PagesModule } from './page/pages.module';
+import { AuthModule } from './auth/auth.module';
 
+// Componentes
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NotpageFoundComponent } from './page/notpage-found/notpage-found.component';
-import { DashboardComponent } from './page/dashboard/dashboard.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { Grafica1Component } from './page/grafica1/grafica1.component';
-import { ProgressComponent } from './page/progress/progress.component';
-import { PagesComponent } from './page/pages.component';
+import { NotpageFoundComponent } from './notpage-found/notpage-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    NotpageFoundComponent,
-    DashboardComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
+    NotpageFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRountingModule
+    AppRountingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
